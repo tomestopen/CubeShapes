6f4469e917a4f79c625373d744503ca6ed1c3aa2
Thomas <tomestmisc@gmail.com>
2023-07-20 19:49:48 +0200

    add CubeShape structure in main.h

--- main.h
+++ main.h
@@ -4,6 +4,15 @@
 	#define CUBE_SHAPES
 	#include <stdlib.h>
 	#include <stdint.h>
+
+	typedef struct sCubeShape {
+			int value;
+			int width;
+			int height;
+			int depth;
+			char *shape;
+		} CubeShape;
+
 	#ifdef BUILD_DLL
 		#include <windows.h>
 		#define DLL_EXPORT __declspec(dllexport)
