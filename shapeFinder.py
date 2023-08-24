4ce400b47460c409a9cd7eddd92d999b327725bc
Thomas <tomestmisc@gmail.com>
2023-08-24 19:22:43 +0200

    add compareShapeArrays, getArchivePath, shapeArrayToList, and loadTextArchive functions to ShapeFinder class

--- shapeFinder.py
+++ shapeFinder.py
@@ -6,12 +6,16 @@ from ctypes import POINTER, pointer, c_char, cast
 from datetime import datetime, timezone
 
 class ShapeFinder:
-	
+
 	def __init__(self):
 		#get the cube shapes library
 		self.CSSL = CubeShapesSL()
 		#find current directory path
 		sourcePath = pathlib.Path(__file__).parent.absolute()
+		#add class function shortcuts
+		self.getArchivePath = ShapeFinder.getArchivePath
+		self.shapeArrayToList = ShapeFinder.shapeArrayToList
+		self.loadTextArchive = ShapeFinder.loadTextArchive
 		#check that the shapes directory exists, create it if it doesn't
 		self.shapeDirPath = os.path.join(sourcePath, "shapes/")
 		if not os.path.isdir(self.shapeDirPath):
@@ -23,8 +27,8 @@ class ShapeFinder:
 		found = False
 		cubeCountP = cubeCount
 		while not found and cubeCountP > 0:
-			shapeFilePath = os.path.join(self.shapeDirPath, "shapes_" + str(cubeCountP) + ".txt")
-			if os.path.isfile(shapeFilePath):
+			shapeFilePath = self.getArchivePath(cubeCountP, self.shapeDirPath)
+			if not shapeFilePath is None:
 				#if the shape file for the current cube count exists, start from there
 				found = True
 				break
@@ -37,35 +41,8 @@ class ShapeFinder:
 				return
 			else:
 				#if a shape file was found for a cube count lower than the required, load it into the source shape list
-				shapeList = []
-				with open(shapeFilePath, "r") as file:
-					lines = list(file)
-					if len(lines[0]) > 0:
-						for line in lines:
-							#each line of the shape file represents a shape object
-							text = line.strip().split(";") #split the line by semicolon
-							shape = [0, 0, 0, 0, []]
-							shape[0] = int(text[0]) #the first element of the line is the shape value
-							shape[1] = int(text[1]) #the second element is the width
-							shape[2] = int(text[2]) #the third element is the height
-							shape[3] = int(text[3]) #the fourth element is the depth
-							#the fifth element is the actual representation of the shape, as a string of ones and zeros
-							for c in text[4]:
-								shape[4].append(int(c))
-							shapeList.append(shape)
-				#once we have loaded all the source shapes, create an array of cube shapes structures and load them with the data
-				sourceShapeCount = len(shapeList)
-				sourceShapeList = (CubeShape * sourceShapeCount)()
-				for i in range(sourceShapeCount):
-					sourceShapeList[i].value = shapeList[i][0]
-					sourceShapeList[i].width = shapeList[i][1]
-					sourceShapeList[i].height = shapeList[i][2]
-					sourceShapeList[i].depth = shapeList[i][3]
-					length = len(shapeList[i][4])
-					sourceShapeList[i].shape = (c_char * length)()
-					shapeByte = bytes(shapeList[i][4])
-					for j in range(length):
-						sourceShapeList[i].shape[j] = shapeByte[j]
+				sourceShapeList = self.loadTextArchive(shapeFilePath)
+				sourceShapeCount = len(sourceShapeList)
 				#adjust the cube count to correspond to the shape being searched for
 				cubeCountP += 1
 		else:
@@ -113,3 +90,99 @@ class ShapeFinder:
 			cubeCountP += 1
 		#clean up the descendent shape array
 		self.CSSL.cleanShapeList(descList, descCount)
+
+	def compareShapeArrays(self, firstShapeArr, secondShapeArr):
+		#this function returns the list of shapes present in the second shape array and missing in the first
+		firstLen = len(firstShapeArr)
+		secondLen = len(secondShapeArr)
+		missingShapeArr = (CubeShape * secondLen)() #the maximum number of missing shapes is the number of shapes in the second array
+		maxBoxSize = (firstShapeArr[0].width + firstShapeArr[0].height + firstShapeArr[0].depth - 2) ** 3
+		for i in range(secondLen):
+			missingShapeArr[i].shape = (c_char * maxBoxSize)()
+		missingLen = self.CSSL.compareShapeLists(firstShapeArr, firstLen, secondShapeArr, secondLen, missingShapeArr)
+		#check whether any shapes were missing
+		if missingLen == 0:
+			#if no shapes were missing return nothing
+			return None
+		#if there were missing shapes, build a final missing shape array and return it
+		missingShapeArrFinal = (CubeShape * missingLen)()
+		for i in range(missingLen):
+			missingShapeArrFinal[i].value = missingShapeArr[i].value
+			missingShapeArrFinal[i].width = missingShapeArr[i].width
+			missingShapeArrFinal[i].height = missingShapeArr[i].height
+			missingShapeArrFinal[i].depth = missingShapeArr[i].depth
+			length = missingShapeArrFinal[i].width * missingShapeArrFinal[i].height * missingShapeArrFinal[i].depth
+			missingShapeArrFinal[i].shape = (c_char * length)()
+			for j in range(length):
+				missingShapeArrFinal[i].shape[j] = missingShapeArr[i].shape[j]
+		return missingShapeArrFinal
+
+	def getArchivePath(cubeCount, shapeDirPath = None):
+		#this function attempts to find the archive file path for the given cube count
+		if shapeDirPath is None:
+			shapeDirPath = os.path.join(pathlib.Path(__file__).parent.absolute(), "shapes/")
+		#build the shape file path
+		shapeFilePath = os.path.join(shapeDirPath, "shapes_" + str(cubeCount) + ".txt")
+		if os.path.isfile(shapeFilePath):
+			#return it if the file exists
+			return shapeFilePath
+		else:
+			#otherwise return nothing
+			return None
+
+	def shapeArrayToList(shapeArray):
+		#this function converts the provided shape array into a list
+		shapeList = []
+		for i in range(len(shapeArray)):
+			shape = [0, 0, 0, 0, []]
+			shape[0] = shapeArray[i].value
+			shape[1] = shapeArray[i].width
+			shape[2] = shapeArray[i].height
+			shape[3] = shapeArray[i].depth
+			size = shapeArray[i].width * shapeArray[i].height * shapeArray[i].depth
+			for j in range(size):
+				shape[4].append(int.from_bytes(shapeArray[i].shape[j], "little"))
+			shapeList.append(shape)
+		return shapeList
+
+	def loadTextArchive(shapeFilePath, asCArray = True):
+		#this function attempts to load the archive file at the specified path and returns a cube shape array if successful
+		if not os.path.isfile(shapeFilePath):
+			#if the shape file does not exist, exit immediately
+			return None
+		shapeList = []
+		with open(shapeFilePath, "r") as file:
+			lines = list(file)
+			if len(lines[0]) > 0:
+				for line in lines:
+					#each line of the shape file represents a shape object
+					text = line.strip().split(";") #split the line by semicolon
+					if len(text) != 5:
+						#if the line is not split  in five, this is an invalid file, exit
+						return None
+					shape = [0, 0, 0, 0, []]
+					shape[0] = int(text[0]) #the first element of the line is the shape value
+					shape[1] = int(text[1]) #the second element is the width
+					shape[2] = int(text[2]) #the third element is the height
+					shape[3] = int(text[3]) #the fourth element is the depth
+					#the fifth element is the actual representation of the shape, as a string of ones and zeros
+					for c in text[4]:
+						shape[4].append(int(c))
+					shapeList.append(shape)
+		#once we have loaded all the source shapes, either create an array of cube shapes structures and load them with the data or return the list
+		if asCArray:
+			sourceShapeCount = len(shapeList)
+			sourceShapeArr = (CubeShape * sourceShapeCount)()
+			for i in range(sourceShapeCount):
+				sourceShapeArr[i].value = shapeList[i][0]
+				sourceShapeArr[i].width = shapeList[i][1]
+				sourceShapeArr[i].height = shapeList[i][2]
+				sourceShapeArr[i].depth = shapeList[i][3]
+				length = len(shapeList[i][4])
+				sourceShapeArr[i].shape = (c_char * length)()
+				shapeByte = bytes(shapeList[i][4])
+				for j in range(length):
+					sourceShapeArr[i].shape[j] = shapeByte[j]
+			return sourceShapeArr
+		else:
+			return shapeList
